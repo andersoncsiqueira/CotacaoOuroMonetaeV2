e66cfbe438962c6bf489294e1d609a8e63d14ea3
Anderson Siqueira <92267959+andersoncsiqueira@users.noreply.github.com>
2025-04-25 14:40:18 -0300

    gold correct

--- api.js
+++ api.js
@@ -1,19 +1,21 @@
 const url = " https://economia.awesomeapi.com.br/last/XAU-USD"
-const urlBr = "https://economia.awesomeapi.com.br/last/USD-BRL"
+const newApiBaseUrl = "https://awesomeapi-proxy-monetae-production.up.railway.app/moeda?base=XAU&target=USD"
+//const urlBr = "https://economia.awesomeapi.com.br/last/USD-BRL"
+const urlBr = "https://awesomeapi-proxy-monetae-production.up.railway.app/moeda?base=USD&target=BRL"
 const inputPrice = document.querySelector("#VG")
 
 
 console.log()
 
-const gold = async (url, urlLastUp) => {
-   const  response = await fetch(url)
+const gold = async (newApiBaseUrl, urlBr) => {
+   const  response = await fetch(newApiBaseUrl)
    const data =  await response.json()
    const troy = (Number(data['XAUUSD'].ask)+Number(data['XAUUSD'].bid))/2
    const grama = troy/31.1035
 
 
 
-   const responseBrl = await fetch(urlLastUp)
+   const responseBrl = await fetch(urlBr)
    
    const datas = await responseBrl.json() 
    const sellPrice = datas['USDBRL'].bid
